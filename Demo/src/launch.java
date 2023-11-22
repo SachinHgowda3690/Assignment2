b5fe78db71193eeefe23bf17edb0d829ea5a5706
Sachin <sachinhgowda00@gmail.com>
2023-11-22 11:24:01 +0530

    First commit

---
+++ Demo/src/launch.java
@@ -0,0 +1,20 @@
+
+public class launch {
+
+	public static void main(String[] args) {
+		
+		int a[]= {10,20,30,40};
+		
+		for(int el:a)
+		{
+			System.out.println(el);
+		}
+		
+		String s1="sachin";
+		int count=s1.toUpperCase().length();
+		
+		System.out.println("the length of the string is :: "+count);
+
+	}
+
+}
